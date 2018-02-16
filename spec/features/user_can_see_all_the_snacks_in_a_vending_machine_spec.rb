89be36dc2fd8309358ebf49c536519d9c499a46d
Luke Chambers <lukenatchambers@gmail.com>
2018-02-16 09:21:50 -0700

    User can see the name of snacks

--- spec/features/user_can_see_all_the_snacks_in_a_vending_machine_spec.rb
+++ spec/features/user_can_see_all_the_snacks_in_a_vending_machine_spec.rb
@@ -1,13 +1,23 @@
 require "rails_helper"
 
 describe "As a User" do
+  before :each do
+    owner = Owner.create!(name: "Opakawagalaga")
+    @vending = owner.machines.create!(location: "Bolivia")
+    snack = Snack.create!(name: "Celery Because That is What is in Vending Machines", price: 800)
+    Stock.create!(machine: @vending, snack: snack)
+  end
   describe "When I visit a specific vending machine page" do
     it "I see the name of all snacks associated with that vending machine" do
-      owner = Owner.new(name: "Opakawagalaga")
-      vending = owner.machine.create!(location: "Bolivia")
-      
-      visit machine_path(vending)
+      visit machine_path(@vending)
 
+      expect(page).to have_content("Celery Because")
+    end
+
+    it "I see the price of all snacks associated with that vending machine" do
+      visit machine_path(@vending)
+
+      expect(page).to have_content("$8.00")
     end
   end
 end
