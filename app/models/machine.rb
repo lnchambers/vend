db2ba9579120bf77be4c081f0e1c847921d4d074
Luke Chambers <lukenatchambers@gmail.com>
2018-02-16 09:36:41 -0700

    Average price for machine is functional

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,4 +2,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :stocks
   has_many :snacks, through: :stocks
+
+  def average_price_of_snacks
+    (snacks.average(:price) / 100).round(2)
+  end
 end
---
+++ spec/features/user_can_see_the_average_price_of_snacks_in_a_machine_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "As a User" do
+  before :each do
+    owner = Owner.create!(name: "Opakawagalaga")
+    @vending = owner.machines.create!(location: "Bolivia")
+    snack = Snack.create!(name: "Celery Because That is What is in Vending Machines", price: 400)
+    snack_2 = Snack.create!(name: "Whiskey Because That is What is in Vending Machines", price: 600)
+    Stock.create!(machine: @vending, snack: snack)
+    Stock.create!(machine: @vending, snack: snack_2)
+  end
+  describe "When I visit a specific vending machine page" do
+    it "I can see the average price of snacks" do
+      visit machine_path(@vending)
+
+      expect(page).to have_content("Average Price: $5.00")
+    end
+  end
+end
